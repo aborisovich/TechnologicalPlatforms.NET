df0cbafbd93c55a95c0805e0daa66a7abef6af99
Andrey Borisovich <borisovich.andrey@gmail.com>
2020-03-29 18:13:15 +0200

    Added FileSystem class and directories browsing tree.

---
+++ TechnologicalPlatforms.NET/Filesystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser
+{
+    public class Filesystem
+    {
+        public DirectoryInfo RootDirectory { get; }
+
+        public Filesystem(string rootDirectoryPath)
+        {
+            if (!Directory.Exists(rootDirectoryPath))
+                throw new DirectoryNotFoundException(rootDirectoryPath);
+            RootDirectory = new DirectoryInfo(rootDirectoryPath);
+        }
+
+        public DirectoryInfo[] GetSubdirectories(string directoryPath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            return directory.GetDirectories("*", SearchOption.TopDirectoryOnly);
+        }
+
+        public FileInfo[] GetFiles(string directoryPath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            return directory.GetFiles("*", SearchOption.TopDirectoryOnly);
+        }
+    }
+}
--- TechnologicalPlatforms.NET/MainWindow.xaml.cs
+++ TechnologicalPlatforms.NET/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +23,8 @@ namespace Browser
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Filesystem filesystem;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +37,59 @@ namespace Browser
 
         private void MenuOpenClick(object sender, RoutedEventArgs eventArgs)
         {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog() { Description = "Select directory to open" };
+            folderDialog.ShowDialog();
+            filesystem = new Filesystem(folderDialog.SelectedPath);
 
+            var rootItem = new TreeViewItem
+            {
+                Header = filesystem.RootDirectory.Name,
+                Tag = filesystem.RootDirectory.FullName
+            };
+
+            Tree.Items.Add(rootItem);
+            CreateItemChildren(rootItem);
+            rootItem.Expanded += OnExpanded;
+        }
+
+        /// <summary>
+        /// Represents single call event reading children of the sender <see cref="TreeViewItem"/>.
+        /// </summary>
+        /// <param name="sender"><see cref="TreeViewItem"/> expanded by user.</param>
+        /// <param name="args">Event args.</param>
+        private void OnExpanded(object sender, RoutedEventArgs args)
+        {
+            foreach (var item in (sender as TreeViewItem).Items)
+                CreateItemChildren(item as TreeViewItem);
+            (sender as TreeViewItem).Expanded -= OnExpanded;
+        }
+
+        /// <summary>
+        /// Creates item children (files and folders) and registers child directories <see cref="TreeViewItem.Expanded"/> event.
+        /// </summary>
+        /// <param name="parent"></param>
+        private void CreateItemChildren(TreeViewItem parent)
+        {
+            if (File.Exists((string)parent.Tag))
+                return;
+
+            foreach (var directory in filesystem.GetSubdirectories(parent.Tag.ToString()))
+            {
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = directory.Name,
+                    Tag = directory.FullName,
+                };
+                item.Expanded += OnExpanded;
+                parent.Items.Add(item);
+            }
+
+            foreach (var file in filesystem.GetFiles(parent.Tag.ToString()))
+                parent.Items.Add(new TreeViewItem
+                {
+                    Header = file.Name,
+                    Tag = file.FullName
+                });
         }
     }
 }
