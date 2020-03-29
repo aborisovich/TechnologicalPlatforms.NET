826b82f0c31c655f4e9b08e676bf2f4ed853a183
Andrey Borisovich <borisovich.andrey@gmail.com>
2020-03-29 22:38:41 +0200

    Created ContextMenu for files and folders with icons. Fixed invocations of event handlers using OriginalSource.

--- TechnologicalPlatforms.NET/MainWindow.xaml.cs
+++ TechnologicalPlatforms.NET/MainWindow.xaml.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -30,14 +30,15 @@ namespace Browser
             InitializeComponent();
         }
 
-        private void MenuExitClick(object sender, RoutedEventArgs eventArgs)
+        protected virtual void MenuExitClick(object sender, RoutedEventArgs eventArgs)
         {
             this.Close();
         }
 
-        private void MenuOpenClick(object sender, RoutedEventArgs eventArgs)
+        protected virtual void MenuOpenClick(object sender, RoutedEventArgs eventArgs)
         {
-            FolderBrowserDialog folderDialog = new FolderBrowserDialog() { Description = "Select directory to open" };
+            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog()
+                { Description = "Select directory to open" };
             folderDialog.ShowDialog();
             filesystem = new Filesystem(folderDialog.SelectedPath);
 
@@ -48,7 +49,7 @@ namespace Browser
             };
 
             Tree.Items.Add(rootItem);
-            CreateItemChildren(rootItem);
+            CreateTreeItemChildren(rootItem);
             rootItem.Expanded += OnExpanded;
         }
 
@@ -57,18 +58,66 @@ namespace Browser
         /// </summary>
         /// <param name="sender"><see cref="TreeViewItem"/> expanded by user.</param>
         /// <param name="args">Event args.</param>
-        private void OnExpanded(object sender, RoutedEventArgs args)
+        protected virtual void OnExpanded(object sender, RoutedEventArgs args)
         {
+            if (sender != args.OriginalSource)
+                return;
             foreach (var item in (sender as TreeViewItem).Items)
-                CreateItemChildren(item as TreeViewItem);
-            (sender as TreeViewItem).Expanded -= OnExpanded;
+                CreateTreeItemChildren(item as TreeViewItem);
+        }
+
+        protected virtual void ContextMenuFolder(object sender, RoutedEventArgs args)
+        {
+            if (sender != args.OriginalSource)
+                return;
+            ContextMenu = new ContextMenu();
+            List<MenuItem> menuItems = new List<MenuItem>()
+            {
+                new MenuItem
+                {
+                    Header = "Create File",
+                    Icon = new Image { Source = new BitmapImage(new Uri(@".\Resources\icons\NewFile_16x.png", UriKind.Relative)) } 
+                },
+                new MenuItem
+                {
+                    Header = "Create Folder",
+                    Icon = new Image { Source = new BitmapImage(new Uri(@".\Resources\icons\FolderOpened_grey_16x.png", UriKind.Relative)) }
+                },
+                new MenuItem
+                {
+                    Header = "Delete Folder",
+                    Icon = new Image { Source = new BitmapImage(new Uri(@".\Resources\icons\Trash_16x.png", UriKind.Relative)) }
+                }
+            };
+            menuItems.ForEach(item => ContextMenu.Items.Add(item));
+        }
+
+        protected virtual void ContextMenuFile(object sender, RoutedEventArgs args)
+        {
+            if (sender != args.OriginalSource)
+                return;
+            ContextMenu = new ContextMenu();
+            List<MenuItem> menuItems = new List<MenuItem>()
+            {
+                new MenuItem
+                {
+                    Header = "Open File",
+                    Icon = new Image { Source = new BitmapImage(new Uri(@".\Resources\icons\OpenFile_16x.png", UriKind.Relative)) }
+                },
+                new MenuItem
+                {
+                    Header = "Delete File",
+                    Icon = new Image { Source = new BitmapImage(new Uri(@".\Resources\icons\Trash_16x.png", UriKind.Relative)) }
+                }
+            };
+            menuItems.ForEach(item => ContextMenu.Items.Add(item));
         }
 
         /// <summary>
         /// Creates item children (files and folders) and registers child directories <see cref="TreeViewItem.Expanded"/> event.
         /// </summary>
-        /// <param name="parent"></param>
-        private void CreateItemChildren(TreeViewItem parent)
+        /// <param name="parent">Parent for whom children shall be found.</param>
+        private void CreateTreeItemChildren(TreeViewItem parent)
         {
             if (File.Exists((string)parent.Tag))
                 return;
@@ -81,15 +130,20 @@ namespace Browser
                     Tag = directory.FullName,
                 };
                 item.Expanded += OnExpanded;
+                item.Selected += ContextMenuFolder;
                 parent.Items.Add(item);
             }
 
             foreach (var file in filesystem.GetFiles(parent.Tag.ToString()))
-                parent.Items.Add(new TreeViewItem
+            {
+                TreeViewItem item = new TreeViewItem
                 {
                     Header = file.Name,
                     Tag = file.FullName
-                });
+                };
+                item.Selected += ContextMenuFile;
+                parent.Items.Add(item);
+            }
         }
     }
 }
